85c2b94b7de2e6d30d1025f6f90fcd835a5aa79f
Santiago Cortes <david.cortes@correounivalle.edu.co>
2024-05-18 13:14:15 -0500

    task-8

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -3,39 +3,48 @@ import { Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
 
 import { Cart } from '../models';
+import { DAOService } from 'src/dao/dao.service';
 
 @Injectable()
 export class CartService {
+  constructor(private readonly daoService: DAOService) {}
+
   private userCarts: Record<string, Cart> = {};
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  async findByUserId(userId: string) {
+    const response:Array<any> = await this.daoService.findCartByUserId(userId)
+
+    console.log(response)
+    return {
+      id: response[0]['id'],
+      items: response.map(row => row['product_id'])
+    }
   }
 
-  createByUserId(userId: string) {
+  async createByUserId(userId: string) {
     const id = v4(v4());
     const userCart = {
       id,
       items: [],
     };
 
-    this.userCarts[ userId ] = userCart;
+    await this.daoService.createCart(userId)
 
     return userCart;
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const userCart = await this.findByUserId(userId);
 
     if (userCart) {
       return userCart;
     }
 
-    return this.createByUserId(userId);
+    return await this.createByUserId(userId);
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, { items }: Cart): Promise<Cart> {
+    const { id, ...rest } = await this.findOrCreateByUserId(userId);
 
     const updatedCart = {
       id,
@@ -48,8 +57,8 @@ export class CartService {
     return { ...updatedCart };
   }
 
-  removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+  async removeByUserId(userId): Promise<void> {
+    await this.daoService.deleteCartByUserId(userId)
   }
 
 }
